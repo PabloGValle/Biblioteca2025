b5f3559e6c432e02b7fa1388cdf3f4d6d68c7cdf
alu17d <alu17d@A204PC03.desarrolloweb.local>
2025-01-13 09:42:04 +0100

    Se coge el proyecto de eduardo de github

--- src/main/java/es/educastur/jla02430/biblioteca2025/Usuario.java
+++ src/main/java/es/educastur/jla02430/biblioteca2025/Usuario.java
@@ -1,60 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package es.educastur.jla02430.biblioteca2025;
 
-
 /**
- * @author Pablo González Valle
+ *
+ * @author eduardo
  */
-
 public class Usuario {
+    private String dni;
+    private String nombre;
+    private String email;
+    private String telefono;
 
-        private String dni;
-        private String nombre;
-        private String email;
-        private String telefono;
+    public Usuario(String dni, String nombre, String email, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
 
-        public Usuario(String dni, String nombre, String email, String telefono) {
-            this.dni = dni;
-            this.nombre = nombre;
-            this.email = email;
-            this.telefono = telefono;
-        }
+    public String getDni() {
+        return dni;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
 
-        public String getDni() {
-            return dni;
-        }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 
-        public void setDni(String dni) {
-            this.dni = dni;
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getTelefono() {
-            return telefono;
-        }
-
-        public void setTelefono(String telefono) {
-            this.telefono = telefono;
-        }
-
-        @Override
-        public String toString() {
-            return dni + " - " + nombre + " - " + email + " - " + telefono;
-        }
-        
-        
+    @Override
+    public String toString() {
+        return dni + "-" + nombre + "-" + email + "-" + telefono;
+    }
 }
